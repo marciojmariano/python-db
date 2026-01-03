54e2c5c5524c14bb5cf76eb59fa51e08c71f70b7
Márcio José Mariano <marciojmariano@gmail.com>
2026-01-03 07:14:59 -0300

    Exemplos e exercícios do fastapi

---
+++ main.py
@@ -0,0 +1,13 @@
+from typing import Union
+
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
